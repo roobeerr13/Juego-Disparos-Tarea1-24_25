2b35374289e123192548a58942e1b1f2c2c5819a
Roberto Jimenez <robertojj2006@gmail.com>
2025-03-29 17:03:27 +0000

    ahora sigo

---
+++ character.py
@@ -0,0 +1,39 @@
+import pygame 
+from entity import Entity
+
+class Character(Entity):
+    def __init__(self, game, width, height, x, y, speed, image_path, dead_image_path):
+        super().__init__(game, width, height, x, y, speed, image_path)
+        self.dead = False
+        self.dead_image = pygame.image.load(dead_image_path)
+
+    def collide(self, other):
+        return self.rect.colliderect(other.rect)
+
+    def collide_with_opponent(self):
+        self.dead = True
+
+    def collide_with_player(self):
+        self.dead = True
+        pygame.time.set_timer(pygame.USEREVENT, 2000)
+
+    def die(self):
+        self.dead = True
+
+    def render(self):
+        if self.dead:
+            self.game.screen.blit(self.dead_image, (self.x, self.y))
+        else:
+            super().render()
+
+    def __str__(self):
+        return super().__str__() + ' (CHARACTER)'
+    
+    def __repr__(self):
+        return self.__str__()
+    
+    def __eq__(self, other):
+        return super().__eq__(other)
+    
+    def __hash__(self):
+        return super().__hash__()
---
+++ entity.py
@@ -0,0 +1,37 @@
+import pygame
+
+class Entity:
+    def __init__(self, game, width, height, x, y, speed, image_path):
+        self.game = game
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+
+    def render(self):
+        self.game.screen.blit(self.image, (self.x, self.y))
+
+    def __str__(self):
+        return f'{self.__class__.__name__}({self.x}, {self.y})'
+    
+    def __repr__(self):
+        return self.__str__()
+    
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+    
+    def __hash__(self):
+        return hash((self.x, self.y))
+    
+    def __lt__(self, other):
+        return self.y < other.y
+    
+    def __le__(self, other):
+        return self.y <= other.y
+    
+    def __gt__(self, other):
+        return self.y > other.y
